6af4266e1340f594d91570905ca6f0b5e3a971b7
miasorrow <manon.arnaud12@gmail.com>
2026-05-05 15:27:36 +0200

    Architecture réparer router ok

--- back/account/urls.py
+++ back/account/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for account project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
---
+++ back/account/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'base.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
