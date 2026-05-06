98adf709ba482d1599f29beabd8ad154708fce3e
Ahmad abo-Alola <ahmadola111@gmail.com>
2026-05-06 15:54:20 +0200

    Déploiement Database dans supadb config dans les settings et reparation du views

--- back/account/settings.py
+++ back/account/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'account.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres.cvdcvlpceydkhtlnwgjt",
+        "PASSWORD": "kfLVOhlu9u5UMWb7",
+        "HOST": "aws-1-eu-central-1.pooler.supabase.com",
+        "PORT": "6543",
+        "OPTIONS": {
+            "sslmode": "require",
+
+        }
     }
 }
+    
 
 
 # Password validation
