db303c89e8996e02ea09c1a9a0044ee3c7747e0f
Ahmad abo-Alola <ahmadola111@gmail.com>
2026-05-07 15:12:51 +0200

    Ajout du domaine Azure dans ALLOWED_HOSTS

--- back/account/settings.py
+++ back/account/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = 'django-insecure-5pq9$*kp*(1^p40vkek&56@x)-n((4n0=@k%khvm_r++)+^^vc
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '*'
+    "169.254.130.2:8000",
+    "localhost",
+    "truite-a5gjbdc8fkesdeh7.francecentral-01.azurewebsites.net",
 ]
 
 
