765f98a499844a8d72ca700a7d1fb0050b6a07ea
Ahmad abo-Alola <ahmadola111@gmail.com>
2026-05-26 13:54:31 +0200

    Add Azure Blob Storage for persistent media files

--- back/truite/settings.py
+++ back/truite/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     "accounts",
     'IA',
+    'storages',
 
 ]
 
@@ -170,6 +171,21 @@ REST_FRAMEWORK = {
 
 
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
 AUTH_USER_MODEL = 'accounts.User'
+
+AZURE_ACCOUNT_NAME = os.getenv("AZURE_ACCOUNT_NAME")
+AZURE_ACCOUNT_KEY = os.getenv("AZURE_ACCOUNT_KEY")
+AZURE_CONTAINER = os.getenv("AZURE_CONTAINER")
+
+AZURE_CUSTOM_DOMAIN = f"{AZURE_ACCOUNT_NAME}.blob.core.windows.net"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.azure_storage.AzureStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+MEDIA_URL = f"https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/"
