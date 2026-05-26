4629232a787ed6e17580a1dfbbdcd41b16f092e6
Ahmad abo-Alola <ahmadola111@gmail.com>
2026-05-26 12:24:15 +0200

    Fix HTTPS media URLs behind Azure proxy

--- back/truite/settings.py
+++ back/truite/settings.py
@@ -41,6 +41,9 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.onrender.com",
 ]
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+USE_X_FORWARDED_HOST = True
+
 
 # Application definition
 
